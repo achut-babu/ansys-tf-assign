df12ed337d6a521ab6283e6cdce8c702c734c2d1
achut-babu <yogeshbhatia@Yogeshs-MBP.modem>
2025-01-27 14:43:36 +1100

    Added default value for the keyname in variable file

--- vars.tf
+++ vars.tf
@@ -9,3 +9,15 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+  default     = "my-key"
+}
