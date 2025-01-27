c2da20005cc07e03e4b5e4cb749bdd25adb054b9
achut-babu <yogeshbhatia@Yogeshs-MBP.modem>
2025-01-27 14:35:52 +1100

    Saved the main.tf for removal of outputs

--- main.tf
+++ main.tf
@@ -206,21 +206,7 @@ root_block_device {
     Environment = "production"
   }
 }
-# Output the instance's public IP and DNS
-output "web_server_public_ip" {
-  value       = aws_instance.web_server.public_ip
-  description = "Public IP address of the web server"
-}
 
-output "web_server_public_dns" {
-  value       = aws_instance.web_server.public_dns
-  description = "Public DNS of the web server"
-}
-
-output "web_server_url" {
-  value       = "http://${aws_instance.web_server.public_dns}"
-  description = "URL of the web server"
-}
 
   
 
